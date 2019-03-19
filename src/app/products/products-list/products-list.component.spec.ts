dc6a09164b4ced389c867ab1e484e4242dd27b6f
lbilde <ljb@easv.dk>
2019-03-19 12:25:02 +0100

    Fixed Product-List Spec file

--- src/app/products/products-list/products-list.component.spec.ts
+++ src/app/products/products-list/products-list.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsListComponent } from './products-list.component';
+import {ProductService} from '../shared/product.service';
+import {FileService} from '../../files/shared/file.service';
+import {Observable, of} from 'rxjs';
+import {Product} from '../shared/product.model';
 
 describe('ProductsListComponent', () => {
   let component: ProductsListComponent;
@@ -8,7 +12,11 @@ describe('ProductsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductsListComponent ]
+      declarations: [ ProductsListComponent ],
+      providers: [
+        {provide: ProductService, useClass: ProductServiceStub},
+        {provide: FileService, useClass: FileServiceStub}
+      ]
     })
     .compileComponents();
   }));
@@ -23,3 +31,11 @@ describe('ProductsListComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+class ProductServiceStub {
+  getProducts(): Observable<Product[]> {
+    return of([]);
+  }
+}
+
+class FileServiceStub {}

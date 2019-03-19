5859a2328788c72d14f6d255e070fe0753e40884
lbilde <ljb@easv.dk>
2019-03-19 10:03:17 +0100

    Fixing H1 Tag issue in our unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to module-test-app!');
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('module-test-app');
   });
 });

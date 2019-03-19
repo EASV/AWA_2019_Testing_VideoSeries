84fada71134f53ca591af72ec78fb7c82b27d3a1
lbilde <ljb@easv.dk>
2019-03-19 13:13:07 +0100

    Simple h2 test added

--- src/app/products/products-list/products-list.component.spec.ts
+++ src/app/products/products-list/products-list.component.spec.ts
@@ -5,6 +5,7 @@ import {ProductService} from '../shared/product.service';
 import {FileService} from '../../files/shared/file.service';
 import {Observable, of} from 'rxjs';
 import {Product} from '../shared/product.model';
+import {By} from '@angular/platform-browser';
 
 describe('ProductsListComponent', () => {
   let component: ProductsListComponent;
@@ -29,6 +30,11 @@ describe('ProductsListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain an h2 tag', () => {
+    const h2Ele = fixture.debugElement.query(By.css('h2'));
+    expect(h2Ele.nativeElement.textContent).toBe('List all Products');
   });
 });
 

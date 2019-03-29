4848bb7e302a6a50b3fab33105897c87937cd5f6
lbilde <ljb@easv.dk>
2019-03-29 10:48:05 +0100

    adding code cova

--- src/app/products/products-list/products-list.component.spec.ts
+++ src/app/products/products-list/products-list.component.spec.ts
@@ -13,7 +13,6 @@ import {Router} from '@angular/router';
 describe('ProductsListComponent', () => {
   let component: ProductsListComponent;
   let fixture: ComponentFixture<ProductsListComponent>;
-  let helper: Helper;
   let dh: DOMHelper<ProductsListComponent>;
   let productServiceMock: any;
   let fileServiceMock: any;
@@ -40,115 +39,137 @@ describe('ProductsListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsListComponent);
     component = fixture.componentInstance;
-    helper = new Helper();
     dh = new DOMHelper(fixture);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Simple HTML', () => {
+    // Simple HTML
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should contain an h2 tag', () => {
+      expect(dh.singleText('h2')).toBe('List all Products');
+    });
+
+    it('Should minimum be one button on the page', () => {
+      expect(dh.count('button')).toBeGreaterThanOrEqual(1);
+    });
+
+    it('Should be a + button first on the page', () => {
+      expect(dh.singleText('button')).toBe('+');
+    });
   });
 
-  it('should contain an h2 tag', () => {
-    expect(dh.singleText('h2')).toBe('List all Products');
+  describe('List Products', () => {
+    let helper: Helper;
+    beforeEach(() => {
+      helper = new Helper();
+    });
+    it('Should show One Unordered List Item', () => {
+      expect(dh.count('ul')).toBe(1);
+    });
+
+    it('Should show no list item when no products are available', () => {
+      expect(dh.count('li')).toBe(0);
+    });
+
+    it('Should show one list item when I have one product', () => {
+      component.products = helper.getProducts(1);
+      fixture.detectChanges();
+      expect(dh.count('li')).toBe(1);
+    });
+
+    it('Should show 100 list item when I have 100 products', () => {
+      component.products = helper.getProducts(100);
+      fixture.detectChanges();
+      expect(dh.count('li')).toBe(100);
+    });
+
+    it('Should show 100 Delete buttons, 1 pr. item', () => {
+      component.products = helper.getProducts(100);
+      fixture.detectChanges();
+      expect(dh.countText('button', 'Delete')).toBe(100);
+    });
+
+    it('Should show 1 span pr product', () => {
+      component.products = helper.getProducts(1);
+      fixture.detectChanges();
+      expect(dh.count('span')).toBe(1);
+    });
+
+    it('Should show 1 product name and id in span', () => {
+      component.products = helper.getProducts(1);
+      fixture.detectChanges();
+      expect(dh.singleText('span'))
+        .toBe(helper.products[0].name + ' -- ' + helper.products[0].id);
+    });
+
+    it('Should show 5 spans, 1 pr. product', () => {
+      component.products = helper.getProducts(5);
+      fixture.detectChanges();
+      expect(dh.count('span')).toBe(5);
+    });
+
+    it('Should show 5 product names and ids in spans', () => {
+      component.products = helper.getProducts(5);
+      fixture.detectChanges();
+      for (let i = 0; i < 5; i++) {
+        const product = helper.products[i];
+        expect(dh.countText('span', product.name + ' -- ' + product.id))
+          .toBe(1);
+      }
+    });
   });
 
-  it('Should minimum be one button on the page', () => {
-    expect(dh.count('button')).toBeGreaterThanOrEqual(1);
+  describe('Delete Products', () => {
+    let helper: Helper;
+    beforeEach(() => {
+      helper = new Helper();
+    });
+    it('Should call deleteProduct once when we click Delete button', () => {
+      component.products = helper.getProducts(1);
+      fixture.detectChanges();
+      spyOn(component, 'deleteProduct');
+      dh.clickButton('Delete');
+      expect(component.deleteProduct).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should call deleteProduct with the product to delete when we click Delete button', () => {
+      component.products = helper.getProducts(1);
+      fixture.detectChanges();
+      spyOn(component, 'deleteProduct');
+      dh.clickButton('Delete');
+      expect(component.deleteProduct).toHaveBeenCalledWith(helper.products[0]);
+    });
   });
 
-  it('Should be a + button first on the page', () => {
-    expect(dh.singleText('button')).toBe('+');
-  });
+  describe('Navigation', () => {
+    let location: Location;
+    let router: Router;
+    beforeEach(() => {
+      location = TestBed.get(Location);
+      router = TestBed.get(Router);
+    });
+    // Navigation
+    it('Should navigate to / before + button click',
+      () => {
+        // find DebugElements with an attached RouterLinkStubDirective
+        expect(location.path()).toBe('');
+      }
+    );
 
-  it('Should navigate to / before + button click',
-    () => {
-      // find DebugElements with an attached RouterLinkStubDirective
-      const location = TestBed.get(Location);
-      expect(location.path()).toBe('');
-    }
-  );
-
-  it('Should navigate to /add on + button click',
-    () => {
-      const router: Router = TestBed.get(Router);
-      spyOn(router, 'navigateByUrl');
-      dh.clickButton('+');
-      expect(router.navigateByUrl).
+    it('Should navigate to /add on + button click',
+      () => {
+        spyOn(router, 'navigateByUrl');
+        dh.clickButton('+');
+        expect(router.navigateByUrl).
         toHaveBeenCalledWith(router.createUrlTree(['/add']),
-        { skipLocationChange: false, replaceUrl: false });
+          { skipLocationChange: false, replaceUrl: false });
+      });
   });
 
-  it('Should show One Unordered List Item', () => {
-    expect(dh.count('ul')).toBe(1);
-  });
-
-  it('Should show no list item when no products are available', () => {
-    expect(dh.count('li')).toBe(0);
-  });
-
-  it('Should show one list item when I have one product', () => {
-    component.products = helper.getProducts(1);
-    fixture.detectChanges();
-    expect(dh.count('li')).toBe(1);
-  });
-
-  it('Should show 100 list item when I have 100 products', () => {
-    component.products = helper.getProducts(100);
-    fixture.detectChanges();
-    expect(dh.count('li')).toBe(100);
-  });
-
-  it('Should show 100 Delete buttons, 1 pr. item', () => {
-    component.products = helper.getProducts(100);
-    fixture.detectChanges();
-    expect(dh.countText('button', 'Delete')).toBe(100);
-  });
-
-  it('Should show 1 span pr product', () => {
-    component.products = helper.getProducts(1);
-    fixture.detectChanges();
-    expect(dh.count('span')).toBe(1);
-  });
-
-  it('Should show 1 product name and id in span', () => {
-    component.products = helper.getProducts(1);
-    fixture.detectChanges();
-    expect(dh.singleText('span'))
-      .toBe(helper.products[0].name + ' -- ' + helper.products[0].id);
-  });
-
-  it('Should show 5 spans, 1 pr. product', () => {
-    component.products = helper.getProducts(5);
-    fixture.detectChanges();
-    expect(dh.count('span')).toBe(5);
-  });
-
-  it('Should show 5 product names and ids in spans', () => {
-    component.products = helper.getProducts(5);
-    fixture.detectChanges();
-    for (let i = 0; i < 5; i++) {
-      const product = helper.products[i];
-      expect(dh.countText('span', product.name + ' -- ' + product.id))
-        .toBe(1);
-    }
-  });
-
-  it('Should call deleteProduct once when we click Delete button', () => {
-    component.products = helper.getProducts(1);
-    fixture.detectChanges();
-    spyOn(component, 'deleteProduct');
-    dh.clickButton('Delete');
-    expect(component.deleteProduct).toHaveBeenCalledTimes(1);
-  });
-
-  it('Should call deleteProduct with the product to delete when we click Delete button', () => {
-    component.products = helper.getProducts(1);
-    fixture.detectChanges();
-    spyOn(component, 'deleteProduct');
-    dh.clickButton('Delete');
-    expect(component.deleteProduct).toHaveBeenCalledWith(helper.products[0]);
-  });
 });
 
 class Helper {
